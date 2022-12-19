cfaad2994970053fa954503f5972bf9051ac6bf1
MartinNiz <martin.niz.niz@gmail.com>
2022-12-19 20:35:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$('button').on('click', function () {
+    let ciudad = document.querySelector("input").value;
+
+    if (ciudad == '') {
+        alert('vacio')
+    }else{
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=95176c8edea30e33338e0eaddd53a916&units=metric&lang=es", function(data) {
+            document.querySelector(".container").style.visibility = "visible";
+            document.querySelector('#temperatura').textContent = Math.trunc(data.main.temp);
+            document.querySelector('#temperatura').innerHTML += '<sup>°C</sup>';
+            document.querySelector('#ciudad').textContent = data.name;
+            document.querySelector('#wicon').src = 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png';
+            document.querySelector('#descripcion').textContent = data.weather[0].description;        
+        }).fail(function () {
+            alert('ciudad no encontrada')
+        })
+        document.querySelector("input").value = ''
+    }
+
+})
